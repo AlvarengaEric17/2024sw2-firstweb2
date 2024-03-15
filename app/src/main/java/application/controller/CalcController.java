1135f8b050fdabcc80e2fc0a3c333b1c9d107344
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-03-15 12:23:36 +0000

    aula2 15032024

---
+++ app/src/main/java/application/controller/CalcController.java
@@ -0,0 +1,23 @@
+package application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class CalcController {
+    @RequestMapping("/calc")
+    public String from() {
+        return "calc/form";
+    }
+
+    @RequestMapping("/soma")
+    public String soma(Model ui,
+        @RequestParam("numA") int nA ,
+        @RequestParam("numB") int nB ){
+        ui.addAttribute("resultado", nA + nB);
+        return "calc/form";
+
+    }
+}
